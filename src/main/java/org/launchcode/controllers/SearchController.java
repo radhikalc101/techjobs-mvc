90304f45b6a8706765dd8fd2ced29c3c231e3eb5
Rsdhika <radheram831@gmail.com>
2018-11-15 18:34:24 -0600

    added search result method implemented joblist

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -23,5 +24,18 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results", method = RequestMethod.POST)
+    public String search(Model model, @RequestParam String searchType,@RequestParam String searchTerm) {
+        ArrayList<HashMap<String, String>> results;
+        model.addAttribute("columns", ListController.columnChoices);
+        if(searchType.equals("all")){
+            results = JobData.findAll();
+        } else {
+            results = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("jobs", results);
+
+        return "search";
+    }
 
 }
